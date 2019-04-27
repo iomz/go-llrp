270654d3ca8613adb6d1ec7ca5c62cd83c7b4232
Iori Mizutani <iori.mizutani@gmail.com>
2019-04-27 13:17:13 +0200

    Rollback NewTagReportDataParam() – leaving out some options

--- tag_report_data.go
+++ tag_report_data.go
@@ -13,27 +13,37 @@ type TagReportData struct {
 }
 
 // BuildTagReportDataParameter takes one Tag struct and build TagReportData parameter payload in []byte
+// FIXME: Take SetReaderConfig or ROSpec configuration parameters
 func NewTagReportDataParam(tag *Tag) []byte {
 	// EPCData
 	// Calculate the right length fro, epc and pcbits
 	epcLengthBits := len(tag.EPC) * 8 // # bytes * 8 = # bits
 	length := 4 + 2 + len(tag.EPC)    // header + epcLengthBits + epc
 	epcd := EPCData(uint16(length), uint16(epcLengthBits), tag.EPC)
-	chIndex := ChannelIndex()
-	timestamp := LastSeenTimestampUTC()
-	tagSeenCount := TagSeenCount()
-	// AirProtocolTagData
-	//aptd := C1G2PC(tag.PCBits)
 
-	tagReportDataLength := 4 + len(epcd) + len(chIndex) + len(timestamp) + len(tagSeenCount) // Rsvd+Type+length->32bits=4bytes
+	// ChannlenIndex
+	chIndex := ChannelIndex()
+
+	// LastSeenTimeStamp
+	timestamp := LastSeenTimestampUTC()
+
+	// TagSeenCount
+	tagSeenCount := TagSeenCount()
+
+	// AirProtocolTagData
+	aptd := C1G2PC(tag.PCBits)
+
+	//tagReportDataLength := 4 + len(epcd) + len(chIndex) + len(timestamp) + len(tagSeenCount) // Rsvd+Type+length->32bits=4bytes
+	tagReportDataLength := len(epcd) + len(aptd) + 4 // Rsvd+Type+length->32bits=4bytes
 
 	// Pack in []byte
 	return Pack([]interface{}{
 		uint16(240),                 // Rsvd+Type=240 (TagReportData parameter)
 		uint16(tagReportDataLength), // Length
 		epcd,
-		chIndex,
-		timestamp,
-		tagSeenCount,
+		//chIndex,
+		//timestamp,
+		//tagSeenCount,
+		aptd,
 	})
 }
